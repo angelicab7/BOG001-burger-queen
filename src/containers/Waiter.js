586ba5261e0c273fae3291a5ff3256b7bffaadfb
Angelica Bonilla Riaño <angelicabonilla723@gmail.com>
2020-11-05 12:31:36 -0500

    some hu2 styles, functionality and responsive

--- src/containers/Waiter.js
+++ src/containers/Waiter.js
@@ -1,11 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Container from '../components/Container';
+
+const tableOptions = [
+  {
+    value: 1,
+    text: '1',
+  },
+  {
+    value: 2,
+    text: '2',
+  },
+  {
+    value: 3,
+    text: '3',
+  },
+  {
+    value: 4,
+    text: '4',
+  },
+  {
+    value: 5,
+    text: '5',
+  },
+];
 
 const Waiter = () => (
-  <section className="inner-page">
-    <div className="first-component margin-t-3">
-      <h1>CHOOSE YOUR OPTION</h1>
-    </div>
-  </section>
+  <Container>
+    <section className="inner-page">
+      <div className="first-component margin-t-3">
+        <h1>CHOOSE YOUR OPTION</h1>
+      </div>
+      <form>
+        <div className=" input-name margin-t-3 first-component">
+          <label htmlFor="user-name" className="letter">
+            Name:
+          </label>
+          <input type="name" name="user-name" className="name-input" />
+        </div>
+        <div className=" input-number first-component">
+          <label htmlFor="table-name" className="first-component letter">
+            Table Number:
+          </label>
+          <select
+            className="select-input"
+            name="table-name"
+            aria-labelledby="table-name"
+          >
+            {tableOptions.map(({ value, text }) => (
+              <option key={value} value={value}>
+                {text}
+              </option>
+            ))}
+          </select>
+        </div>
+      </form>
+    </section>
+  </Container>
 );
 
 export default Waiter;
